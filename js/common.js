8104b02da1101b2fc0475d54de3041bf78b4bc52
Maxon <maxon1989.pro@gmail.com>
2016-08-05 04:45:19 +0300

    Осталась форма поиска, скроллинг, адаптация

--- js/common.js
+++ js/common.js
@@ -9,7 +9,32 @@ $(function() {
 
 	$('.slick-slider-discount').slick({
 		accessibility: true,
-		arrows: true
+		arrows: true,
+		slidesToShow: 7
+	});
+
+	$('.slick-slider-discount-brand').slick({
+		accessibility: true,
+		arrows: true,
+		slidesToShow: 7
+	});
+
+	$('.slick-slider-discount-news').slick({
+		accessibility: true,
+		arrows: true,
+		slidesToShow: 3
+	});
+
+	$('.slick-slider-discount-themed').slick({
+		accessibility: true,
+		arrows: true,
+		slidesToShow: 7
+	});
+
+	$('.slick-slider-discount-hits').slick({
+		accessibility: true,
+		arrows: true,
+		slidesToShow: 5
 	});
 
 	//tabs BRAND
@@ -20,6 +45,54 @@ $(function() {
 	}).eq(0).addClass("active");
 
 
+	// hover card
+	$('.line-cart').mouseover(function(){
+		$(".cart_list_hidden").css("display"," block");
+	});
+	$('.cart_list_hidden').mouseout(function(){
+		$('.cart_list_hidden').hide();
+	});
+	$(document).mouseup(function (e) {
+		var container = $(".cart_list_hidden");
+		if (container.has(e.target).length === 0){
+			container.hide();
+		}
+	});
+
+	// hover nav
+	$('.top-line-nav nav > ul > li:last-child').mouseover(function(){
+		$(".drop-line-nav").css("display"," block");
+		$('.top-line-nav nav > ul > li:last-child').find('a').first().css("color","#ff0000");
+	});
+	$('.drop-line-nav').mouseout(function(){
+		$('.drop-line-nav').hide();
+		$('.top-line-nav nav > ul > li:last-child').find('a').first().css("color","#3c3c3c");
+	});
+	$(document).mouseup(function (e) {
+		var container = $(".drop-line-nav");
+		if (container.has(e.target).length === 0){
+			container.hide();
+		}
+	});
+
+
+	// close block
+	$( ".button_close" ).click(function() {
+		$('.hidden_bg').css("display"," none");
+	});
+
+	// open hidden blocks
+	$( ".top-line-region" ).click(function() {
+		$('#regions-hidden').css("display"," block");
+		$('.hidden_bg').css("display"," block");
+	});
+
+	// open hidden blocks
+	$( ".top-line-call a" ).click(function() {
+		$('#success_hidden').css("display"," block");
+		$('.hidden_bg').css("display"," block");
+	});
+
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
@@ -27,23 +100,6 @@ $(function() {
 		});
 	};
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
 
 	//Chrome Smooth Scroll
 	try {
@@ -57,11 +113,4 @@ $(function() {
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 	
-});
-
-$(window).load(function() {
-
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-
 });
